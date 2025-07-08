a1cb2c6c337375730cf9b138a76ffeb6d6628023
KousalyaK03 <kousalya.kadimella@gmail.com>
2025-07-08 14:30:54 -0700

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.py
@@ -0,0 +1,11 @@
+# Last updated: 7/8/2025, 2:30:53 PM
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        gas = 0
+        for n in nums:
+            if gas < 0:
+                return False
+            elif n > gas:
+                gas = n
+            gas -= 1
+        return True
