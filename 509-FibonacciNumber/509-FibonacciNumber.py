dfc9b698f41358baac5f37db5569628a7c03908b
KousalyaK03 <kousalya.kadimella@gmail.com>
2025-07-17 15:13:34 -0700

    File updated at 7/17/2025, 3:13:34 PM

--- 509-FibonacciNumber/509-FibonacciNumber.py
+++ 509-FibonacciNumber/509-FibonacciNumber.py
@@ -1,27 +1,6 @@
-# Last updated: 7/17/2025, 3:02:24 PM
+# Last updated: 7/17/2025, 3:13:34 PM
 class Solution:
     def fib(self, n: int, memo = {}) -> int:
-        if n <= 1:
-            return n
-        dp = [0] * (n + 1)
-        dp[1] = 1
-        
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[n]
-
-"""
-        if memo is None:
-            memo = {}
-
-        if n <= 1:
-            return n
-        if n in memo:
-            return memo[n]
-        memo[n] = self.fib(n - 1, memo) + self.fib(n - 2, memo)
-        return memo[n]
-
-
 
         if n <= 1:
             return n
@@ -34,3 +13,30 @@ class Solution:
         return prev1
 
 """
+Tabulation (Bottom Up approach):
+TC: O(n) SC: O(n) 
+
+        if n <= 1:
+            return n
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+
+Memoization (Top Down approach):
+TC: O(n) SC: O(n)
+        if memo is None:
+            memo = {}
+
+        if n <= 1:
+            return n
+        if n in memo:
+            return memo[n]
+        memo[n] = self.fib(n - 1, memo) + self.fib(n - 2, memo)
+        return memo[n]
+
+"""
+
+        
